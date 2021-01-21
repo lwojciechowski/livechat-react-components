e6806d0fd51fd36e358af04ee9a75f49fe7fdbcc
Łukasz Wojciechowski <nostrzak@gmail.com>
2021-01-21 16:28:23 +0100

    Add new components

---
+++ src/components/Auth.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useRef } from "react";
+import { accountsSdk } from "@livechat/accounts-sdk";
+
+import Loading from "./Loading";
+
+const Auth = ({ children, signIn, clientId }) => {
+    const [loading, setLoading] = useState(true);
+    const [auth, setAuth] = useState(null);
+    const authInstance = useRef(null);
+
+    useEffect(() => {
+        authInstance.current = accountsSdk.init({
+            client_id: clientId,
+            onIdentityFetched: (error, data) => {
+                setLoading(false);
+                if (data) {
+                    setAuth(data);
+                    authRef.token = data.access_token;
+                }
+                if (error) {
+                    console.error(error);
+                }
+            }
+        });
+    }, [clientId]);
+
+    if (loading) {
+        return (
+            <div>
+                <Loading />
+            </div>
+        );
+    }
+
+    return (
+        <AuthContext.Provider value={auth}>
+            {auth !== null ? children : signIn(authInstance)}
+        </AuthContext.Provider>
+    );
+};
+
+export const AuthContext = React.createContext(null);
+export default Auth;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Loading } from './components/Loading';
-
+export { default as LoginWithLivechat } from './components/LoginWithLivechat';
+export { default as Auth, AuthContext } from './components/Auth';
